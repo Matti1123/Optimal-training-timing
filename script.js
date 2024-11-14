4f102e701512e943b1163899d06af28e05e778f7
Matti1123 <163666169+Matti1123@users.noreply.github.com>
2024-11-14 22:23:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optionales JavaScript, um bei Bedarf später Dynamik hinzuzufügen
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Seite ist geladen und bereit.");
+});
