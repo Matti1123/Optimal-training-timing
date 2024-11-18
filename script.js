4cf09da4da122b144785542218dfb37d538b1dbe
matti0101 <fm9023@mci4me.at>
2024-11-18 16:27:18 +0100

    deleted code

--- script.js
+++
@@ -1,4 +0,0 @@
-// Optionales JavaScript, um bei Bedarf später Dynamik hinzuzufügen
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Seite ist geladen und bereit.");
-});
