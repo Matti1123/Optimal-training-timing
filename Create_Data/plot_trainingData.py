bcdea2aadb002360e883d6c122b500428f3d7501
Flo6020 <jf4659@mci4me.at>
2024-11-20 14:47:03 +0100

    fix

--- Create_Data/plot_trainingData.py
+++ Create_Data/plot_trainingData.py
@@ -10,11 +10,10 @@ plt.figure(figsize=(10, 6))
 for column in data.columns[1:]:  # Assuming the first column is an index or non-numeric
     plt.plot(data.iloc[:, 0], data[column], label=column)
 
-plt.xlabel('Index')
-plt.ylabel('Values')
+plt.xlabel('hours')
+plt.ylabel('wheight in kg')
 plt.title('Training Data Plot')
 plt.legend()
 plt.grid(True)
-plt.show()
-'Data_Set/training_ip.png'
 plt.savefig('Data_Set/training_ip.png')
+plt.show()
