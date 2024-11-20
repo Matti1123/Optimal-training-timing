fc765151848dce10eff9b42f1fe234cce3719516
Flo6020 <jf4659@mci4me.at>
2024-11-20 15:20:26 +0100

    fatigue mit random werten

--- Analyse_Data/fftttest.py
+++ Analyse_Data/fftttest.py
@@ -9,12 +9,16 @@ tau1 = 42  # Zeitkonstante für Fitness (längere Erholung)
 tau2 = 7     # Zeitkonstante für Müdigkeit (schneller Abbau)
 
 n = np.arange(0, 30*24)
-w = np.zeros(30*24)  # Trainingseinheiten
-w[0] = 1
-w[48] = 1
-w[96] = 1
-w[150] = 1
-w[190] = 1
+w = np.zeros(30*24)  # Trainingseinheiten, 30 Tage * 24 Stunden
+w[0] = 1  # Erste Trainingseinheit
+
+# Generiere 99 zufällige Trainingseinheiten (1-24 Stunden Erhöhung)
+for i in range(1, 100):
+    prev_time = np.where(w == 1)[-1][-1]  # Hole die letzte Trainingseinheit
+    random_increase = np.random.randint(1, 25)  # Zufällige Erhöhung zwischen 1 und 24 Stunden
+    next_time = prev_time + random_increase  # Berechne die nächste Trainingseinheit
+    if next_time < len(w):  # Sicherstellen, dass wir nicht aus dem Array hinausgehen
+        w[next_time] = 1
 
 # Fitness, Müdigkeit und Performance berechnen
 y1_fitness = k1 * np.array([sum(w[i] * np.exp(-(n_j - i) / tau1) for i in range(n_j)) for n_j in n])
@@ -39,7 +43,6 @@ print(max_index, max_value + 2, f"({max_index}, {max_value:.2f})")
 plt.xlabel("Zeit in Tagen")
 plt.ylabel("Leistung")
 plt.title("Fatigue-Fitness-Modell: Nettoleistung, Fitness und Müdigkeit")
-#plt.savefig("Analyse_Data\tests_fitness_fatigue.py")
 plt.legend()
 plt.grid(True)
 
